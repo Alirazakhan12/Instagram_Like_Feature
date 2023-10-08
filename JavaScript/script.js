8df1302c23a27e049ea75bf5337bf36b4f0f2210
Alirazakhan12 <raza346863@gmail.com>
2023-10-08 14:28:43 +0500

    update

---
+++ JavaScript/script.js
@@ -0,0 +1,38 @@
+
+function instafeature1(){
+ let icon = document.querySelector("#icon")
+  icon.style.scale = "1.30"
+  icon.style.opacity = "1"
+  icon.style.transition = "0.40s"
+  
+  setTimeout(function(){
+  icon.style.scale = "1"
+   icon.style.opacity = "0"
+  },
+  1500)
+ }
+
+ let icon2 = document.querySelector("#icon2")
+function instafeature2(){
+  icon2.style.scale = "1.30"
+  icon2.style.opacity = "1"
+  icon2.style.transition = "0.40s"
+
+  setTimeout(function(){
+  icon2.style.scale = "1"
+   icon2.style.opacity = "0"
+  },
+  1500)
+ }
+ let icon3 = document.querySelector("#icon3")
+function instafeature3(){
+  icon3.style.scale = "1.30"
+  icon3.style.opacity = "1"
+  icon3.style.transition = "0.40s"
+
+  setTimeout(function(){
+  icon3.style.scale = "1"
+   icon3.style.opacity = "0"
+  },
+  1500)
+ }
